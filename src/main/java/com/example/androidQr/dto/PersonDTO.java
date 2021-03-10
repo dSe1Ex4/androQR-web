cf84887a10b8f20d3abf8ecd451692c283559653
Koval Alexander <koval@hostco.ru>
2021-03-10 11:23:22 +0500

    Добавить Rest методы без бд

---
+++ src/main/java/com/example/androidQr/dto/PersonDTO.java
@@ -0,0 +1,14 @@
+package com.example.androidQr.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class PersonDTO {
+
+  private String firstName;
+  private String lastName;
+  private String middleName;
+  private String event;
+}
--- src/main/java/com/example/androidQr/dto/UserDTO.java
+++ src/main/java/com/example/androidQr/dto/UserDTO.java
@@ -1,14 +1,12 @@
 package com.example.androidQr.dto;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 public class UserDTO {
 
-  private String firstName;
-  private String lastName;
-  private String middleName;
-  private String event;
+  private String login;
+  private String password;
+  private String session;
+
 }
