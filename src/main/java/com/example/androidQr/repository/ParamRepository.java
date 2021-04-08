783484a9d7dea3282ca2dc67490325510fa23363
dSe1Ex4 <starg99@mail.ru>
2021-04-08 17:22:43 +0500

    Рефакторинг
    Изменена версия приложения
    Удалено лишнее

--- src/main/java/com/example/androidQr/repository/ParamRepository.java
+++ src/main/java/com/example/androidQr/repository/ParamRepository.java
@@ -2,7 +2,6 @@ package com.example.androidQr.repository;
 
 import com.example.androidQr.entity.Param;
 import java.util.List;
-import jdk.jfr.EventSettings;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
